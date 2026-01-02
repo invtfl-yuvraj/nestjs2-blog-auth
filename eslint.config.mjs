dcff5b01894480fa4c5f53cd85170dbbe8de6919
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2026-01-02 19:27:23 +0530

    fix: updated the eslint config file with custom settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,28 +8,37 @@ export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
+
+  // base configs
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+
   {
     languageOptions: {
+      parser: tseslint.parser,      // Fix unsafe decorator calls
+      sourceType: 'commonjs',
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
       parserOptions: {
-        projectService: true,
+        project: ['./tsconfig.json'], // Ensure correct type checking
         tsconfigRootDir: import.meta.dirname,
       },
     },
   },
+
+  // Custom rules
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['warn', { endOfLine: 'auto' }],
+      '@typescript-eslint/no-unsafe-call': 'warn', 
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn'
     },
   },
 );
