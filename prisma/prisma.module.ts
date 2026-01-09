8b372e289374257a00fefb4850e77029ee8db245
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2026-01-09 23:40:10 +0530

    refactor: added tags field in post schema

--- prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Global() // makes Prisma available everywhere without re-import
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
-export class PrismaModule {}
